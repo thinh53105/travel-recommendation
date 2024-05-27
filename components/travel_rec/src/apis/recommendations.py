9c1efc68c890d09a827ecbf1708d55eb448dd232
thinh5 <thinh53105@gmail.com>
2024-05-27 20:15:31 +0700

    wrap response with JSONResponse

--- components/travel_rec/src/apis/recommendations.py
+++ components/travel_rec/src/apis/recommendations.py
@@ -2,6 +2,7 @@
 Handle get recommendations request
 """
 from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
 
 from ..controllers.recommendations import process_recommendation, get_results
 from ..schemas.recommendations import GetResultsParams, SearchParams
@@ -13,11 +14,13 @@ recommendations_router = APIRouter(
 
 @recommendations_router.get('/')
 async def get_recommendations(params: SearchParams = Depends(SearchParams)):
-    return await process_recommendation(
+    result = await process_recommendation(
         country=params.country,
         season=params.season
     )
+    return JSONResponse(result, status_code=200)
 
 @recommendations_router.get('/results')
 async def get_recommendation_results(params: GetResultsParams = Depends(GetResultsParams)):
-    return await get_results(rec_id=params.id)
+    result = await get_results(rec_id=params.id)
+    return JSONResponse(result, status_code=200)
